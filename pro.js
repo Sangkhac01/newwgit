6c5ee0d7df6bbe7f78f0bf45430d3c177a13d178
Sangkhac01 <sangkhac01@gmail.com>
2023-05-07 12:59:35 +0700

    bot mess

---
+++ pro.js
@@ -0,0 +1,14 @@
+[
+    "Mod lại Bởi Nguyễn Khắc Sang",
+    "hiiiiii",
+    "Anh Sang ơi em yêu anh vãi lồn em nứng lắm rồi",
+    "Chào cậu chủ ạ :3",
+    "Liên hệ ad qua zalo 0376026556",
+    "Bé xin chào ngài ạ UwU",
+    "Chào mừng cậu chủ trở lại",
+    "Chào mừng ngài trở lại ạ",
+    "Em yêu anh Sang",
+    "Sang ơi em yêu anh",
+    "Chúc anh iu một ngày tốt lành",
+    "Ơ Ơ em nứng quá anh Sang ơi"
+ ]
